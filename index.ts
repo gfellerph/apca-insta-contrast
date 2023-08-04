5dd67d6fb54f1f8500e4e6aa3189a0609b40983a
Philipp Gfeller <philipp.gfeller@post.ch>
2023-08-04 22:17:58 +0200

    init

---
+++ index.ts
@@ -0,0 +1,28 @@
+import { APCAcontrast, sRGBtoY } from "apca-w3";
+import { colorParsley } from "colorparsley";
+
+const whiteY = sRGBtoY([255, 255, 255]);
+const blackY = sRGBtoY([0, 0, 0]);
+
+const setColor = (header: HTMLHeadingElement, input: HTMLInputElement) => {
+  const bgColor = input.value;
+  header.style.setProperty("--background-color", bgColor);
+  const yBgColor = sRGBtoY(colorParsley(bgColor));
+  const whiteContrast = Math.abs(APCAcontrast(whiteY, yBgColor) as number);
+  const blackContrast = Math.abs(APCAcontrast(blackY, yBgColor) as number);
+  console.log(whiteContrast, blackContrast);
+  header.style.setProperty(
+    "--text-color",
+    whiteContrast > blackContrast ? "white" : "black"
+  );
+};
+
+window.addEventListener("load", () => {
+  const colorInput: HTMLInputElement | null = document.querySelector(".picker");
+  const header = document.querySelector("h1");
+
+  if (!colorInput || !header) return;
+
+  colorInput.addEventListener("input", (e) => setColor(header, colorInput));
+  setColor(header, colorInput);
+});
